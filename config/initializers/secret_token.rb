97580fbd7bc2aac33e78ed9b2adf3c9283910f96
Rit Li <rit@quietdynamite.com>
2010-11-09 16:36:33 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HungryPig::Application.config.secret_token = 'bc7b488dd0e6b6856048d930a11b09651ffba56c972cea785e0b418ef0beaf75293a6ae26968df5800bac1770fc7f52356e271fe3e96d829a91de20dad587b18'
