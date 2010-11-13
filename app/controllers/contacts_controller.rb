8743a308f0a6b1a5fddcbfa8532886637f02c91f
Rit Li <rit@quietdynamite.com>
2010-11-13 10:19:54 -0800

    Add update action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,19 @@
 class ContactsController < ApplicationController
   def index
-    contacts = Contact.all.to_json
+    contacts = Contact.all.to_json(:only => [:id, :first_name, :last_name])
     contacts = JSON(contacts)
     render :json => { :contacts => contacts }
+  end
+
+  def update
+    contact = Contact.find(params[:id])
+    if contact.update_attributes(params[:contact])
+      contact = contact.to_json(:only => [:id, :first_name, :last_name])
+      cantact = JSON(contact)
+      render :json => { :contact => contact }
+    else
+      errors = Hash.new.merge(contact.errors)
+      render :json => { :errors => contact.errors }
+    end
   end
 end
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe ContactsController do
-
+  context "#update" do
+    it 'updates the contact with the params' do
+      contact = Contact.create(:first_name => 'Yo', :last_name => 'Ming')
+      post :update, :id => contact.id, :contact => {:first_name => 'Yao'}
+      Contact.find(contact.id).first_name.should == 'Yao'
+    end
+  end
 end
